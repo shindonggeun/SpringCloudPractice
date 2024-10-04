1bebefb5ff5783656da37fa8dc383c0cb4d3ae0a
신동근 <tlsehdrms124@naver.com>
2024-10-04 15:02:32 +0900

    feat: Inventory-Service 초기 설정

---
+++ Inventory-Service/src/main/java/com/example/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,17 @@
+package com.example.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+}
