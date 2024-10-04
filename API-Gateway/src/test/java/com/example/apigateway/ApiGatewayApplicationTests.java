17300d15c1dffb1f8d9b13daf48da2993b3bc073
신동근 <tlsehdrms124@naver.com>
2024-10-04 14:34:19 +0900

    feat: Spring Cloud API Gateway 초기 설정 및 폴더 구조 재설정

--- API-Gateway/src/test/java/com/example/apigateway/ApiGatewayApplicationTests.java
+++ API-Gateway/src/test/java/com/example/apigateway/ApiGatewayApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApigatewayApplicationTests {
+class ApiGatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
